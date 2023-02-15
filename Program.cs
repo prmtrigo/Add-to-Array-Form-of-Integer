d651abc4c048771a58523cf6bc3e5f5ffb309db0
Paulo <trigopaulo@gmail.com>
2023-02-15 10:09:46 -0300

    problem solved

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Add_to_Array_Form_of_Integer;
+class Program
+{
+    public IList<int> AdicionarNumero(int[] array, int numero){
+
+        int tamanho = array.Length -1;
+
+        var resultado = new List<int>();
+
+        while(tamanho >=0 || numero > 0){
+
+            numero += (tamanho >= 0 ? array[tamanho--] : 0);
+
+            resultado.Add((numero % 10));
+
+            numero /= 10;
+
+        }
+
+        resultado.Reverse();
+
+        return resultado;
+    }
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+
+        int[] array = {1,2,0,0};
+
+        int numero = 34;
+
+        Console.WriteLine(p.AdicionarNumero(array,numero));
+
+    }
+}
